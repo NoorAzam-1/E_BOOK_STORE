ce7231a406a90be0639a96f5e73854b0cee9a5b6
NoorAzam-1 <azamnoor698@gmail.com>
2026-03-23 17:47:47 +0530

    data page

---
+++ src/data/site.js
@@ -0,0 +1,92 @@
+export const site = {
+  brand: "E_BOOK_STORE",
+  hero: {
+    badge: "Book of the Month",
+    title: "The Midnight Stargazer",
+    description:
+      "An evocative journey through the constellations of memory and the architecture of the human heart. Curated for the restless soul.",
+    price: 14.99,
+    image:
+      "/images/heroImage.png",
+  },
+  genres: [
+    {
+      name: "Fiction",
+      count: "15,400+ titles available",
+      tags: ["Historical", "Fantasy", "Mystery"],
+      image:
+        "/images/t1.png",
+      spotlight: {
+        title: "Silence of the Void",
+        author: "Marcus Thorne",
+        image:
+          "/images/books/t1Book.png",
+      },
+    },
+    {
+      name: "Tech",
+      count: "8,200+ titles available",
+      tags: ["Programming", "AI & ML", "Design"],
+      image:
+        "/images/t2.png",
+      spotlight: {
+        title: "The Digital Nomad",
+        author: "Elena Rodriguez",
+        image:
+          "/images/books/t2Book.png",
+      },
+    },
+    {
+      name: "Business",
+      count: "12,100+ titles available",
+      tags: ["Startup", "Strategy", "Finance"],
+      image:
+        "/images/t3.png",
+      spotlight: {
+        title: "Pulse of Innovation",
+        author: "Sarah Jenkins",
+        image:
+          "/images/books/t3Book.png",
+      },
+    },
+  ],
+  arrivals: [
+    {
+      title: "The Digital Nomad",
+      author: "Elena Rodriguez",
+      price: 12.99,
+      rating: 4.5,
+      reviews: 128,
+      image:
+        "https://lh3.googleusercontent.com/aida-public/AB6AXuDWvCIbe7FXTplXFl-Z6yC7f7wOsfa71sWVpWGfWOMD_tPDHp6S0coWZVfFNSMWBCfwRGH4hgwUQOg-Dp567w3HunSGvBomGp_vDwF9D8Mu84FOKAzLA-qRvN7182jGIcqu1M3JrXBW_X5vRPunlVwvMlrgwqGG0XHPqxEfTk6mLVbSfceFyR1EhiBOO7jnpi-g1MySUAWGz5PfMbcIw-X7qE7W66OBsctWTjoCRdtN7J13nc0fbHfRWWNZAuqCQEZMn1hpIM99g0g",
+    },
+    {
+      title: "Silence of the Void",
+      author: "Marcus Thorne",
+      price: 18.5,
+      rating: 5,
+      reviews: 340,
+      image:
+        "https://lh3.googleusercontent.com/aida-public/AB6AXuBvBT-EdMP2Mrnl9bSdRTeB9A9_9dJiFngKRghMyn3-UuehAaVrBn9Je6OmdH6tAnzJIiXlfYHa1sPgkZFQQib0r52WNWIjxThCnrJySKLHTS59xexjjLGJ3b2TO6g1yWV7iqTtYKOVrdtgXrq2-oYf-0p6h2UJZ9bi4aTBOEECSX9L0NX7JbFSkPB5JSW5X_K8Jc8ojEr5LEY95FjoEcrNvld1JQFKDJtMbqZAJH3s6scnhS18VwS32mic84DPlin4G36Fmg8oKL0",
+    },
+    {
+      title: "Architecture of Dreams",
+      author: "Julian Vane",
+      price: 15.0,
+      rating: 4,
+      reviews: 89,
+      image:
+        "https://lh3.googleusercontent.com/aida-public/AB6AXuCRqtLbkiVDR2Fg2sBWm0yJQnN8vxpiqWcpYzMZ4JlkNpCgTczurxowlSBjTaTVp-S5LXZjAlqYqkFKFeexclRUyU3GXYrTKAlsmhaw4HaiDkuFQFsEfxDVvuPgxkP0K9DpqBPG0Ds3vX_-Jn4m1VUH_XBceSDtQnbqh18ANI-EXEfH_Ct-kupKdCjcS3iditgyiKLmiIeq9WR-2KC_gttWSUspsW0Yt4svzDJDL2h6KNfGIm4w5CpKYB7JWQEZT7H12SM51x0QXZw",
+    },
+    {
+      title: "Pulse of Innovation",
+      author: "Sarah Jenkins",
+      price: 21.99,
+      rating: 4,
+      reviews: 215,
+      image:
+        "https://lh3.googleusercontent.com/aida-public/AB6AXuBsu6tklvvhw7nsQpNVsaZFMC4SRJQBH6euuyj4KHAXbBL2ggU98goEZcMLgLzWKfA7lKkOx0C-YqIZQquczmtJ3lHRdB0wsBjzug8o9JxkGl8-HUdlILPbIOBck0p_E8kuJxDN_sQldLzL7wBITkbWOHvnUwiI4ya-6v1GYbSGx0_d6-t-JVa46XKoWBamw2HOgd7Tq6WImh69qxTYNGz51nbxeYUYvwuJEmvqm-sznYD8QoK1gB9kpO69RHzrfX0y0ReQuQFcbEc",
+    },
+  ],
+  footerLinks: ["Journal", "Privacy", "Terms", "Shipping"],
+};
