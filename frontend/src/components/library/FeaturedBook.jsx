ffe05344ab7e9493c36e7b9db4ccbabf78221b56
NoorAzam-1 <azamnoor698@gmail.com>
2026-03-26 18:51:02 +0530

    created FeatureBook Components for Library page

---
+++ frontend/src/components/library/FeaturedBook.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function FeaturedBook() {
+  return (
+    <div className="group w-full bg-surface-container-low p-6 rounded-2xl flex flex-col md:flex-row gap-6 items-center cursor-pointer border border-transparent hover:border-white/10 ">
+      <div className="p-6 bg-gray-200 rounded-sm group-hover:scale-105 transition duration-300 md:min-w-[43%]">
+        <Image
+        alt="images"
+        src="/images/alcheminst.jfif"
+        height={300}
+        width={300}
+        className="w-40 sm:w-48 h-64 object-cover rounded shadow-xl"
+      />
+      </div>
+
+      <div >
+        <p className="text-primary text-xs uppercase">Continue Reading</p>
+        <h3 className="text-2xl font-bold mt-2">The Alchemist</h3>
+        <p className="text-sm text-on-surface-variant mb-4">
+          Paulo Coelho • 84%
+        </p>
+
+        <div className="bg-surface-container-highest h-2 rounded-full">
+          <div className="bg-primary h-2 w-[84%]" />
+        </div>
+
+        <p className="text-sm italic mt-4 opacity-70">
+          &quot;And, when you want something, all the universe conspires in helping you to achieve...&quot;
+        </p>
+      </div>
+    </div>
+  );
+}
