54ac66180b01444efe159de2bcce93b0a4d4faf2
priyak02125 <priyaroy.sanoj@gmail.com>
2026-03-27 18:43:23 +0530

    Implement product update functionality and enhance user registration with address and contact fields

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {addProduct,listProduct,removeProduct, singleProduct} from "../controllers/productController.js"
+import {addProduct,listProduct,removeProduct, singleProduct,updateProduct} from "../controllers/productController.js"
 import upload from '../middleware/multer.js';
 
 const productRouter = express.Router ();
@@ -8,5 +8,7 @@ productRouter.post('/add', upload.array('images', 10), addProduct);
 productRouter.post('/single',singleProduct);
 productRouter.post('/remove',removeProduct);
 productRouter.get('/list',listProduct);
+
+productRouter.put('/update/:id', upload.array('images', 10), updateProduct);
 
 export default productRouter
