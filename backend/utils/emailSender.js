4484b757b6bed8423d6629a89956a77b504d8c6c
priyak02125 <priyaroy.sanoj@gmail.com>
2026-03-30 18:19:14 +0530

    Refactor email sending functionality to use Mailtrap and remove nodemailer configuration

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -1,22 +1,47 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: 587,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendEmail = async (to, subject, html) => {
+//   const mailOptions = {
+//     from: `"Ebook Store" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+import { MailtrapClient } from "mailtrap";
+
+const client = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
 });
 
 export const sendEmail = async (to, subject, html) => {
-  const mailOptions = {
-    from: `"Ebook Store" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  };
+  try {
+    const response = await client.send({
+      from: {
+        email: process.env.MAILTRAP_SENDER_EMAIL,
+        name: "Ebook Store",
+      },
+      to: [{ email: to }],
+      subject: subject,
+      html: html,
+      category: "Ebook Store",
+    });
 
-  await transporter.sendMail(mailOptions);
+    return response;
+  } catch (error) {
+    console.error("Email Error:", error);
+    throw error;
+  }
 };
