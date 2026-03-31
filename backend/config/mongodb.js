82447f7b3e70934baf160c3dfdeb13cacc559136
NoorAzam-1 <azamnoor698@gmail.com>
2026-03-31 18:47:58 +0530

    Format mongodb connection code: fix indentation and consistency

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
- mongoose.connection.on('connected',()=>{
+  mongoose.connection.on("connected", () => {
     console.log("DB Connected");
- })
+  });
 
- await mongoose.connect(`${process.env.MONGODB_URI}`) 
-
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
+};
 export default connectDB;
