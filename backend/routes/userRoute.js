c89f3d09504c1a0e5ef773ef23052b5fba979501
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-01 18:08:56 +0530

    feat: add new endpoints and refactor user routes for better API structure

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,22 @@
-import express from 'express';
-import {loginUser,registerUser,adminLogin,forgotPassword,resetPassword,userProfile} from '../controllers/userController.js' 
-import userAuth from '../middleware/userAuth.js';
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  adminLogin,
+  forgotPassword,
+  resetPassword,
+  userProfile,
+  logoutUser,
+} from "../controllers/userController.js";
+import userAuth from "../middleware/userAuth.js";
 
 const userRouter = express.Router();
-userRouter.post('/forgot_password', forgotPassword);
-userRouter.post('/reset_password', resetPassword);
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-userRouter.post('/admin',adminLogin)
+userRouter.post("/forgot_password", forgotPassword);
+userRouter.post("/reset_password", resetPassword);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/logout", logoutUser);
+userRouter.post("/admin", adminLogin);
 
 userRouter.get("/profile", userAuth, userProfile);
 
