47e0ec7893c62b7f2bac8652c25531779fc3fa44
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-01 18:08:36 +0530

    refactor: update user authentication middleware with improved security checks

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -3,36 +3,24 @@ import userModel from "../models/userModel.js";
 
 const userAuth = async (req, res, next) => {
   try {
-    console.log("Checking user authentication...",req.headers.authorization);
-    const token = req.headers.authorization?.split(" ")[1];
-    console.log("token from header", token);
+    const token = req.cookies.token;
 
     if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Not Authorized, please login",
-      });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await userModel.findById(decoded.id).select("-password -__v");
+    const user = await userModel.findById(decoded.id).select("-password");
 
     if (!user) {
-      return res.status(401).json({
-        success: false,
-        message: "User not found",
-      });
+      return res.status(401).json({ message: "User not found" });
     }
 
     req.user = user;
-
     next();
   } catch (error) {
-    res.status(401).json({
-      success: false,
-      message: "Invalid token, please login again",
-    });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
