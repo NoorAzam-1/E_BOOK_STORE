9ee648a745d95b3f32b9b064ee687a4c6ef7f970
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-02 12:34:10 +0530

    refactor: improve axios formatting and add multipart form-data header for addProduct

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -48,7 +48,6 @@ axiosInstance.resetPassword = (data) =>
 
 axiosInstance.getProfile = () => axiosInstance.get("/api/user/profile");
 
-
 //feedback
 axiosInstance.addFeedback = (data) =>
   axiosInstance.post("/api/feedback/add", data);
@@ -59,20 +58,23 @@ axiosInstance.getSingleFeedback = (id) =>
   axiosInstance.get(`/api/feedback/${id}`);
 
 axiosInstance.updateFeedback = (id, data) =>
-axiosInstance.put(`/api/feedback/update/${id}`, data);
+  axiosInstance.put(`/api/feedback/update/${id}`, data);
 
 axiosInstance.deleteFeedback = (id) =>
-axiosInstance.delete(`/api/feedback/delete/${id}`);
+  axiosInstance.delete(`/api/feedback/delete/${id}`);
 
 //  PRODUCT APIs
-axiosInstance.getAllProducts = () =>
-  axiosInstance.get("/api/product/all");
+axiosInstance.getAllProducts = () => axiosInstance.get("/api/product/all");
 
 axiosInstance.getSingleProduct = (id) =>
   axiosInstance.get(`/api/product/${id}`);
 
 axiosInstance.addProduct = (data) =>
-  axiosInstance.post("/api/product/add", data);
+  axiosInstance.post("/api/product/add", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
 axiosInstance.updateProduct = (id, data) =>
   axiosInstance.put(`/api/product/update/${id}`, data);
@@ -80,27 +82,22 @@ axiosInstance.updateProduct = (id, data) =>
 axiosInstance.deleteProduct = (id) =>
   axiosInstance.delete(`/api/product/delete/${id}`);
 
-
 // WISHLIST APIs
 axiosInstance.addWishlist = (data) =>
   axiosInstance.post("/api/wishlist/add", data);
 
-axiosInstance.getWishlist = () =>
-  axiosInstance.get("/api/wishlist/all");
+axiosInstance.getWishlist = () => axiosInstance.get("/api/wishlist/all");
 
 axiosInstance.getSingleWishlist = (id) =>
   axiosInstance.get(`/api/wishlist/${id}`);
 
 axiosInstance.deleteWishlist = (id) =>
- axiosInstance.delete(`/api/wishlist/delete/${id}`);
-
+  axiosInstance.delete(`/api/wishlist/delete/${id}`);
 
 //  CART APIs
-axiosInstance.addToCart = (data) =>
-  axiosInstance.post("/api/cart/add", data);
+axiosInstance.addToCart = (data) => axiosInstance.post("/api/cart/add", data);
 
-axiosInstance.getCart = () =>
-  axiosInstance.get("/api/cart");
+axiosInstance.getCart = () => axiosInstance.get("/api/cart");
 
 axiosInstance.updateCart = (data) =>
   axiosInstance.put("/api/cart/update", data);
@@ -108,18 +105,15 @@ axiosInstance.updateCart = (data) =>
 axiosInstance.removeFromCart = (productId) =>
   axiosInstance.delete(`/api/cart/remove/${productId}`);
 
-axiosInstance.clearCart = () =>
-  axiosInstance.delete("/api/cart/clear");
+axiosInstance.clearCart = () => axiosInstance.delete("/api/cart/clear");
 
 // ORDER APIs
 axiosInstance.placeOrder = (data) =>
   axiosInstance.post("/api/order/place", data);
 
-axiosInstance.getMyOrders = () =>
-  axiosInstance.get("/api/order/my");
+axiosInstance.getMyOrders = () => axiosInstance.get("/api/order/my");
 
-axiosInstance.getSingleOrder = (id) =>
-  axiosInstance.get(`/api/order/${id}`);
+axiosInstance.getSingleOrder = (id) => axiosInstance.get(`/api/order/${id}`);
 
 axiosInstance.updateOrderStatus = (id, data) =>
   axiosInstance.put(`/api/order/update/${id}`, data);
