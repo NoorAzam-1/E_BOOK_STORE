ea6f9d61f8c9178a56904db4fdc0e889e2d4aeb9
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-06 15:03:35 +0530

    updated book grid comp

--- frontend/src/components/browse/BookGrid.jsx
+++ frontend/src/components/browse/BookGrid.jsx
@@ -1,9 +1,8 @@
 import BookCard from "./BookCard";
-// frontend\src\components\browse\BookGrid.jsx
 
 export default function BookGrid({ books }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-10">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
       {books.length > 0 ? (
         books.map((book) => (
           <BookCard key={book._id} book={book} />
