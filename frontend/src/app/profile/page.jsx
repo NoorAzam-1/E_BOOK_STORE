0e6ff2425d3b8515a92f009fff3716e01c4a4efe
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-09 18:02:51 +0530

    profile

--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -1,12 +1,5 @@
 import ProfilePage from "@/components/profile";
-import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function Profile() {
-  return (
-    <>
-      <ProtectedRoute>
-        <ProfilePage />
-      </ProtectedRoute>
-    </>
-  );
+  return <ProfilePage />;
 }
