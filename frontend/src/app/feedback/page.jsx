8ba561ed8c184829b3feb0ba76a593faaa3ea345
NoorAzam-1 <azamnoor698@gmail.com>
2026-04-10 13:16:29 +0530

    feedabck error solved

--- frontend/src/app/feedback/page.jsx
+++ frontend/src/app/feedback/page.jsx
@@ -5,7 +5,7 @@ import FeedbackList from "@/components/feedback/FeedbackList";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
+    <div className="min-h-screen p-6">
       <h1 className="text-2xl font-bold text-center mb-6">
         Feedback System
       </h1>
